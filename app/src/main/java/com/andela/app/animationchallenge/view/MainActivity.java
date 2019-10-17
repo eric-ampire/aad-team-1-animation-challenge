f9ce9abb4a5d578608b2cf2228b766d50ce932b5
Eric Ampire <ericampire.top@gmail.com>
2019-10-17 15:22:39 +0200

    [UPDATE] Refactoring and adding todo list

--- app/src/main/java/com/andela/app/animationchallenge/view/MainActivity.java
+++ app/src/main/java/com/andela/app/animationchallenge/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.andela.app.animationchallenge;
+package com.andela.app.animationchallenge.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.andela.app.animationchallenge.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO: Check if the user is already connected
+        if(false) {
+            // TODO: Switch to SignInActivity
+        }
     }
 }
