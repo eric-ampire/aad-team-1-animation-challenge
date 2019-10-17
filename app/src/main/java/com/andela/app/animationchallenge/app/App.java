44b4408f3ab2db005be8670953a145bd1edd56ce
Eric Ampire <ericampire.top@gmail.com>
2019-10-17 14:42:31 +0200

    Adding App class

---
+++ app/src/main/java/com/andela/app/animationchallenge/app/App.java
@@ -0,0 +1,10 @@
+package com.andela.app.animationchallenge.app;
+
+import android.app.Application;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
