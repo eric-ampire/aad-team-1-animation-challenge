cd5e4d85c911d601d65dc7d3b1be99ab96cad974
Edrisa A. Turay <eturay@iom.int>
2019-10-17 11:30:12 +0200

    Changed origin

--- app/src/main/java/com/andela/app/animationchallenge/SignInActivity.java
+++ app/src/main/java/com/andela/app/animationchallenge/SignInActivity.java
@@ -2,6 +2,7 @@ package com.andela.app.animationchallenge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,17 +14,26 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
 
     private EditText mUsername, mPassword;
     private Button mSignin;
-    private TextView mSignup;
+    private TextView mSignup, mWelcomeMessage;
+    private Intent intent;
+    private int CALLING_PAGE_CODE;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        handleIntents();
         initComponents();
     }
-
+    //Handle the intent and get the page code
     private void handleIntents(){
-
+        intent =  getIntent();
+        if(intent.hasExtra("SIGN_IN_CODE")){
+            CALLING_PAGE_CODE = intent.getIntExtra("SIGN_IN_CODE", 0001);
+        }else{
+            CALLING_PAGE_CODE = 0001;
+        }
     }
+
     private void initComponents(){
         mUsername = findViewById(R.id.et_username);
         mPassword = findViewById(R.id.et_password);
@@ -31,6 +41,8 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
         mSignin.setOnClickListener(this);
         mSignup = findViewById(R.id.tv_signup);
         mSignup.setOnClickListener(this);
+        mWelcomeMessage = findViewById(R.id.tv_welcome_message);
+        mWelcomeMessage.setText("Welcome " + ((CALLING_PAGE_CODE == 0001) ? "Doctor" : "Patient"));
     }
 
     @Override
@@ -41,6 +53,7 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
             case  R.id.tv_signup : mSignupButtonClicked();
         }
     }
+
     private  void mSigninButtonClicked(){
         Toast.makeText(SignInActivity.this, "Ooh, i did something", Toast.LENGTH_LONG).show();
     }
@@ -48,6 +61,4 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
     private void mSignupButtonClicked(){
 
     }
-
-
 }
